49982d3045186d89662bc8bb94f18fa2b8e081f5
fullstack300622 <108460728+fullstack300622@users.noreply.github.com>
2022-08-29 21:24:51 +0300

    Create classCode1.sql

---
+++ classCode1.sql
@@ -0,0 +1,11 @@
+-- INSERT INTO students (name, last_name)
+-- VALUES ('Sarah', 'Conor'), ('Zeu', 'Mazemshane');
+
+select id,
+name, 
+last_name from students;
+
+
+-- select * from students;
+
+-- delete from students where id=2;
